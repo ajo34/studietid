e8cbdab7d5af2bd4b5b00d2f819faf785aed02ac
ajo34 <addavadda@gmail.com>
2024-09-03 14:32:17 +0200

    check for unique email function, remove person function

--- studietid/app.js
+++ studietid/app.js
@@ -1,22 +1,35 @@
 const sqlite3 = require('better-sqlite3')
 const db = sqlite3('./studietid.db', {verbose: console.log})
 
-let result = addUser("fnam", "lnam", 1, 1, "mal")
+
+//let result = addUser("fname", "lnam", 1, 1, "momal")
+let deleted = deleteUser('mal')
 
 function addUser(firstName, lastName, idRole, isAdmin, email)
  {
-
-    let sql = db.prepare(`INSERT INTO user (firstName, lastName, idRole, isAdmin, email)  
-                          values (?, ?, ?, ?, ?)`)
-    const info = sql.run(firstName, lastName, idRole, isAdmin, email)
-    
-    sql = db.prepare(`
-        SELECT user.id as userid, firstname, lastname, role.name as role 
-        FROM user inner join role on user.idrole = role.id 
-        WHERE user.id  = ?`);
-    let rows = sql.all(info.lastInsertRowid)  
-    console.log("rows.length",rows.length)
-
-    return rows[0]
+    if(!eMailDunExist(email)){
+        let sql = db.prepare(`INSERT INTO user (firstName, lastName, idRole, isAdmin, email)  
+                            values (?, ?, ?, ?, ?)`)
+        const info = sql.run(firstName, lastName, idRole, isAdmin, email)
+        
+        sql = db.prepare(`
+            SELECT user.id as userid, firstname, lastname, email, role.name as role 
+            FROM user inner join role on user.idrole = role.id 
+            WHERE user.id  = ?`);
+        let rows = sql.all(info.lastInsertRowid)  
+        console.log("rows.length",rows)
+        return rows[0]
+    }
 }
 
+function eMailDunExist(email) {
+    let sql=db.prepare(`SELECT user.id FROM USER WHERE email = ?`);
+    const info = sql.get(email)
+    console.log('infolop:', info)
+    return info
+}
+
+function deleteUser(email){
+    let sql=db.prepare(`DELETE FROM USER WHERE email = ?`);
+    const info = sql.run(email)
+}
