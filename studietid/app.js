853f088315c4417b0310003293188f3d8a3ecfa0
ajo34 <addavadda@gmail.com>
2024-09-06 10:31:35 +0200

    deletes useractivity with user, and made confirmingActivity function.

--- studietid/app.js
+++ studietid/app.js
@@ -2,12 +2,13 @@ const sqlite3 = require('better-sqlite3')
 const db = sqlite3('./studietid.db', {verbose: console.log})
 
 
-//let result = addUser("fname", "lnam", 1, 1, "momal")
-let deleted = deleteUser('mal')
-
+//let result = addUser("fname", "lnam", 1, 1, "mo@amal")
+//deleted = deleteUser('mo@amal')
+//let activ = regActivity(63, getFormattedDate(), 3, 2, 2)
+console.log(getFormattedDate())
 function addUser(firstName, lastName, idRole, isAdmin, email)
  {
-    if(!eMailDunExist(email)){
+    if(!eMailCheck(email)){
         let sql = db.prepare(`INSERT INTO user (firstName, lastName, idRole, isAdmin, email)  
                             values (?, ?, ?, ?, ?)`)
         const info = sql.run(firstName, lastName, idRole, isAdmin, email)
@@ -22,14 +23,39 @@ function addUser(firstName, lastName, idRole, isAdmin, email)
     }
 }
 
-function eMailDunExist(email) {
-    let sql=db.prepare(`SELECT user.id FROM USER WHERE email = ?`);
-    const info = sql.get(email)
-    console.log('infolop:', info)
-    return info
+function eMailCheck(email) {
+    if(email.includes('@')) {
+        let sql=db.prepare(`SELECT user.id FROM USER WHERE email = ?`);
+        const info = sql.get(email)
+        console.log('infolop:', info)
+        return info
+    } else {return true}
 }
 
 function deleteUser(email){
-    let sql=db.prepare(`DELETE FROM USER WHERE email = ?`);
-    const info = sql.run(email)
+    let sql=db.prepare(`SELECT user.id FROM USER WHERE email = ?`);
+    let info = sql.get(email).id
+    let temp = info
+    console.log('AAAAAAAA:', info)
+    sql=db.prepare(`DELETE FROM activity WHERE idUser = ?`);
+    info = sql.run(temp)
+    sql=db.prepare(`DELETE FROM USER WHERE email = ?`);
+    info = sql.run(email)
 }
+function getFormattedDate() {
+    return new Date().toISOString().replace('T', ' ').slice(0, 19);
+}
+
+function regActivity(idUser, startTime, idSubject, idRoom, idStatus){
+    let sql = db.prepare(`INSERT INTO activity (idUser, startTime, idSubject, idRoom, idStatus)
+                        values (?, ?, ?, ?, ?)`)
+    const info = sql.run(idUser, startTime, idSubject, idRoom, idStatus)
+    console.log(info)
+}
+function confirmingActivity(yes, duration, idUser, startTime,  idStatus){
+    let sql = db.prepare(`UPDATE activity SET idStatus = ?, duration = ? WHERE idUser = ? AND startTime = ? AND idStatus = ?`)
+    const info = sql.run(yes, duration, idUser, startTime,  idStatus)
+    console.log(info)
+}
+confirmingActivity(3, 60, 3, '2024-09-05 07:13:56', 2)
+
